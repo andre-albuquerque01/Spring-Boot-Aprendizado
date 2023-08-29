782a21d3e01f28928e3f4e1d54f954a93df6ff0d
André Albuquerque <97198432+andre-albuquerque01@users.noreply.github.com>
2023-08-28 21:25:41 -0300

    Up da faculdade

---
+++ SpringBoot - Aprendizado/springjoin/springjoin/src/main/java/br/senac/df/springjoin/SpringjoinApplication.java
@@ -0,0 +1,13 @@
+package br.senac.df.springjoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjoinApplication.class, args);
+	}
+
+}
